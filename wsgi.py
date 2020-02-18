e4d9bd3e6bfa4883ad284b51078e12bb02edb0f3
Jack Wanitkun <wanitkun_cat@cat.pdx.edu>
2020-02-18 02:41:10 -0800

    Add survey

--- wsgi.py
+++ wsgi.py
@@ -18,6 +18,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ohl.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "survey.settings")
 
 application = get_wsgi_application()
