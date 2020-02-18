c3da573253bf1131940b1d566c3daf2b13c20c88
Jack Wanitkun <wanitkun_cat@cat.pdx.edu>
2020-02-17 19:18:50 -0800

    V21

--- settings.py
+++ settings.py
@@ -21,7 +21,7 @@ print(
 )
 
 # ALLOWED_HOST = ["https://6e4c5806.ngrok.io","http://127.0.0.1:8000/survey/"]
-ALLOWED_HOSTS = ['https://ohl1.herokuapp.com/']
+ALLOWED_HOSTS = ['*'] #https://ohl1.herokuapp.com/
 DEBUG = False
 ROOT = os.path.dirname(os.path.abspath(__file__))
 CSV_DIR = os.path.join(ROOT, "csv")
@@ -192,6 +192,3 @@ LOGGING = {
     },
 }
 
-
-import django_heroku
-django_heroku.settings(locals())
